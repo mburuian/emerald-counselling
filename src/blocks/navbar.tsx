744a84fb2fcb536d6967b73557592b8e673c8f43
Brian <gamerfunky78@gmail.com>
2025-03-23 13:04:47 +0300

    remove anchor tag due to next.js uild policies.

--- src/blocks/navbar.tsx
+++ src/blocks/navbar.tsx
@@ -78,9 +78,9 @@ export function NavigationMenuDemo() {
                     <ul className="grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
                     <li className="row-span-3">
                         <NavigationMenuLink asChild>
-                        <a
+                        <div
                             className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
-                            href="/"
+                            
                         >
                             <Icons.logo className="h-6 w-6" />
                             <div className="mb-2 mt-4 text-lg font-medium">
@@ -90,7 +90,7 @@ export function NavigationMenuDemo() {
                             Beautifully designed components built with Radix UI and
                             Tailwind CSS.
                             </p>
-                        </a>
+                        </div>
                         </NavigationMenuLink>
                     </li>
                     <ListItem href="/docs" title="Introduction">
