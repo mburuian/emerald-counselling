45de9d097744bdcddf9cf8f8281f58480e19bb95
Brian <gamerfunky78@gmail.com>
2025-05-14 15:54:35 +0300

    add props for better versality.

--- src/blocks/landing.jsx
+++ src/blocks/landing.jsx
@@ -13,14 +13,14 @@ export default function Landing(){
                     <br />
                     <p className='nunito'>Discover how our compassionate counselling can change your life or better, Experience the tranormative  journey o mental wellness with emerald counselling ,where we provide personalised sessions tailored to your unque nedds  whether you seek individual sguidance ,group support, marital improvement or amily solutions , our dedicated proessionals are here to acilitate Meaningul conversations and healing</p>
                     <br />
-                    <Button/>
+                    <Button content="Request a Quota"/>
                 </div>
                 <div className='w-3/6 ' id='container1'>
                     <div className='container2 p-3 flex flex-col items-end justify-end'>
                         <div className='glassmorphism p-3 flex flex-col items-end justify-end'>
                             <p className='nunito text-end'>Discover the transformative power of tailored counselling services designed to meet your unique needs. Emerald Counselling specializes in individual, group, marital, and family support, ensuring that every session is focused on your personal journey toward healing and growth. Our compassionate professionals are here to provide a safe space where you can express your thoughts and feelings freely. </p>
                             <br />
-                            <Button/>
+                            <Button content="Get Started."/>
                         </div>
                     </div>
                 </div>
